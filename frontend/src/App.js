f764012d37899641e60bc0bb62a6a0a6d0f1a5c4
Abdallahman Habyarimana <habdallah005@gmail.com>
2025-11-20 17:26:54 -0500

    update npm dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component {
+  constructor() {
+    super();
+      this.state = {
+        user: {
+          id: '',
+          name: '',
+        }
+      
+    }
+  }
+
+  loadUser = () => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        id: '1',
+        name: 'Abdallah'
+      }
+    })
+
+   
+  }
+  render() {
+     console.log(this.state.user)
+    return(
+      <div className='container'>
+      <h1>Hello world</h1>
+
+      <button onClick={this.loadUser}>Click Me</button>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
